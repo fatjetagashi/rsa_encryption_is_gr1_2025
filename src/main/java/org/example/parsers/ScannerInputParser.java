db66b590dbbb99d3b80f28ad51379f3c1e55b1b8
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-10-10 23:25:03 +0200

    Added basic Parser for input via terminal using Scanner System in

---
+++ src/main/java/org/example/parsers/ScannerInputParser.java
@@ -0,0 +1,18 @@
+package org.example.parsers;
+
+import java.util.Scanner;
+
+public class ScannerInputParser {
+
+  private final Scanner sc;
+
+  public ScannerInputParser() {
+    this.sc = new Scanner(System.in);
+  }
+
+  public String parse() {
+    return "";
+  }
+
+
+}
